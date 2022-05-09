1f889cd112e72bce6280e3c0c893356f6602f884
Bexultan Alipov <madi.alpov123@gmail.com>
2022-05-09 15:25:51 +0200

    Changed Text

--- pickerwheel.py
+++ pickerwheel.py
@@ -3,7 +3,7 @@ print("What do you want?")
 Craving = input()
 
 
-list = ["Lucky you!!!", "No, you will go out for 10 minute \n take some fresh breath", "No, you will do what you suppose to do but after 10 minute take a break", "No, Dont be so pussy, whole live in your hand, dont give up!!!" ]
+list = ["Lucky you!!! \n Do what ever you wanted to do but you have an 1 hour", "No, you will go out for 10 minute \n take some fresh breath", "No, you will do what you suppose to do but after 10 minute take a break", "No, Dont be so pussy, whole live in your hand, dont give up!!!" ]
 
 rndomint = random.randint(0, len(list) - 1)
 print(rndomint)
